5add86d23c474e2a5d8cdc5accf5cdf93120a154
lasheymusic1-eng <lasheymusic1@gmail.com>
2026-07-06 16:14:56 -0700

    feat: alo

--- src/utils/limiter.js
+++ src/utils/limiter.js
@@ -2,7 +2,7 @@ import { rateLimit } from "express-rate-limit";
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 7,
+  limit: 10,
   standardHeaders: "draft-8",
   legacyHeaders: false,
   ipv6Subnet: 56,
